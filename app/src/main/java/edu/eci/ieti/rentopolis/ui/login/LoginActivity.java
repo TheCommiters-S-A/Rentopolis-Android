66aaf09cd5e8a72fe13643837233334948358e79
Davor Javier Cortés Cardozo <davorcortes.dc@gmail.com>
2021-05-13 14:22:51 -0500

    login activity events

--- app/src/main/java/edu/eci/ieti/rentopolis/ui/login/LoginActivity.java
+++ app/src/main/java/edu/eci/ieti/rentopolis/ui/login/LoginActivity.java
@@ -1,131 +1,117 @@
 package edu.eci.ieti.rentopolis.ui.login;
 
 import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
+import android.text.TextUtils;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import edu.eci.ieti.rentopolis.NavigationDrawer;
 import edu.eci.ieti.rentopolis.R;
-import edu.eci.ieti.rentopolis.ui.login.LoginViewModel;
-import edu.eci.ieti.rentopolis.ui.login.LoginViewModelFactory;
+
 
 public class LoginActivity extends AppCompatActivity {
 
-    private LoginViewModel loginViewModel;
+    private FirebaseAuth mAuth;
+
+    private Button loginButton;
+    private EditText emailView;
+    private EditText passwordView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
+        emailView = (EditText) findViewById(R.id.username);
+        passwordView= (EditText) findViewById(R.id.password);
+        loginButton = (Button) findViewById(R.id.login);
+        mAuth = FirebaseAuth.getInstance();
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+    }
 
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
+    public void onLogin(View view){
+
+        emailView.setError(null);
+        passwordView.setError(null);
+        String email = emailView.getText().toString();
+        String password  = emailView.getText().toString();
+        if(validCredentials(email, password)){
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(LoginActivity.this, NavigationDrawer.class));
+                        finish();
+                    } else {
+                        showInvalidDataError();
+                    }
                 }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
-            }
-        });
+            });
+        }else{
+            Toast.makeText(LoginActivity.this, "Verifica los Campos", Toast.LENGTH_SHORT).show();
+        }
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+    }
+
+    private void showInvalidDataError(){
+
+        runOnUiThread(new Runnable() {
             @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                finish();
-            }
-        });
-
-        TextWatcher afterTextChangedListener = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ignore
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // ignore
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+            public void run() {
+                emailView.setError("Verify Your email!");
+                passwordView.setError("verify Your Password!");
             }
         });
     }
 
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+    private boolean validCredentials(String email, String password){
+
+        View focusView = null;
+        if (!isEmailValid(email)) {
+            emailView.setError(getString(R.string.error_invalid_email));
+            focusView = emailView;
+        }
+        if (!isPasswordValid(password)) {
+            passwordView.setError(getString(R.string.error_invalid_password));
+            focusView = passwordView;
+        }
+        if (TextUtils.isEmpty(email)) {
+            emailView.setError(getString(R.string.error_field_required));
+            focusView = emailView;
+        }
+        if (TextUtils.isEmpty(password)) {
+            passwordView.setError(getString(R.string.error_field_required));
+            focusView = emailView;
+        }
+
+        if (focusView != null) {
+            focusView.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 
-    private void showLoginFailed(@StringRes Integer errorString) {
-        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    private boolean isEmailValid(String email) {
+        /**Si la cadena contiene el caracter @ es un email valido*/
+        return email.contains("@");
     }
+
+    private boolean isPasswordValid(String password) {
+        /**Si la cadena supera los 4 caracteres es una contraseña valida*/
+        return password.length() > 5;
+    }
+
 }
